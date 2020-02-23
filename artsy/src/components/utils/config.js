988792e7af47e606a24138a3e787f561889542c5
amlane <twelvexstring@gmail.com>
2020-02-23 16:31:33 -0600

    updated flash of previous photo on single photo view

--- artsy/src/components/utils/config.js
+++ artsy/src/components/utils/config.js
@@ -4,6 +4,7 @@ const NODE_ENV = process.env.REACT_APP_FORCE_NODE_ENV || process.env.NODE_ENV;
 
 if (NODE_ENV === "development") {
   baseURL = "http://localhost:4000/api";
+  // baseURL = "https://artsy-be.herokuapp.com/api";
 } else if (NODE_ENV === "production") {
   baseURL = "https://artsy-be.herokuapp.com/api";
 }
